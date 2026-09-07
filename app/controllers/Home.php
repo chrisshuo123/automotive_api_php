293509c8de3a078c18c8fbd757bc4e69028ad4d4
chrisshuo123 <chr1z100@gmail.com>
2026-09-07 20:30:36 +0700

    In controllers Home::index() method, revise the view from 'index' to 'user/index' directory
    move controller Crud::insert() into this controller Home::insert() method, where insert page acts as user page who want to contribute to our car list

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,6 +3,16 @@ class Home extends Controller {
     public function index() {
         // echo 'home/index';
         echo 'home page';
-        $this->view('index');
+        $this->view('user/index');
+        // For the models already being called via Crud::getCars() method globally to script.js
+    }
+
+    public function insert() {
+        $data['judul'] = "Insert Car User Panel";
+        $data['carList'] = $this->model('Home_model')->getAllCars() ?: []; // Pastikan array kosong kalau null
+        $data['merekList'] = $this->model('Home_model')->getAllMerek() ?: []; // Pastikan array kosong kalau null
+        $data['jenisList'] = $this->model('Home_model')->getAllJenis() ?: []; // Pastikan array kosong kalau null
+        echo 'insert page (user panel)';
+        $this->view('user/insert', $data);
     }
 }
