3415d91ebadd847dafe0105aae9ec2381df23e93
chrisshuo123 <chr1z100@gmail.com>
2026-09-09 05:06:14 +0700

    edit the app/config/config.php descriptions

--- app/config/config.php
+++ app/config/config.php
@@ -11,13 +11,13 @@ define('DB_USER', 'postgres');
 define('DB_PASS', 'root');
 
 // Base URL (without trailing slash)
-// define('BASEURL', 'http://localhost/shidokan-web/admin/public');
-// define('BASEURL', 'http://localhost/shidokan-web/user/public');
+// define('BASEURL', 'http://localhost/automotive_api/admin/public');
+// define('BASEURL', 'http://localhost/automotive_api/user/public');
 define('BASEURL', 'http://localhost/automotive_api_php/public');
 
 // Define Paths
-define('APP_PATH', dirname(__DIR__)); // /path/to/shidokan-web/app
-define('PUBLIC_PATH', dirname(APP_PATH) . '/public'); // /path/to/shidokan-web/public
+define('APP_PATH', dirname(__DIR__)); // /path/to/automotive_api/app
+define('PUBLIC_PATH', dirname(APP_PATH) . '/public'); // /path/to/automotive_api/public
 
 // No need to define section-specific URLs here
 // They will be handled in the routing
